0a474becdcd5f91b9c6201f5efe36a7ab3b31531
petros <pit.trak@gmail.com>
2020-09-05 19:49:13 +0300

    Added ErrorDetails

--- src/main/java/com/petros/springboot2jpacrud/exception/ErrorDetails.java
+++ src/main/java/com/petros/springboot2jpacrud/exception/ErrorDetails.java
@@ -5,10 +5,42 @@
  */
 package com.petros.springboot2jpacrud.exception;
 
+import java.util.Date;
+
 /**
  *
  * @author petros
  */
 public class ErrorDetails {
+    /*
+        The default error response provided by Spring Boot 
+        contains all the details that are typically needed.
+        However, you might want to create a framework inde-
+        pendent response structure for your organization. 
+        In that case, you can define a specific error 
+        response structure.
+    */
     
+    private Date timestamp;
+    private String message;
+    private String details;
+    
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+    
+    public Date getTimestamp() {
+        return timestamp;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public String getDetails() {
+        return details;
+    }
 }
