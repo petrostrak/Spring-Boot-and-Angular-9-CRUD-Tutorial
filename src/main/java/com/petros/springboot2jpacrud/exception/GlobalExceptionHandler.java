45f46dca93f8ee7d94a01ce6226398dfc47f17e1
petros <pit.trak@gmail.com>
2020-09-05 19:57:05 +0300

    Added GlobalExceptionHandler

--- src/main/java/com/petros/springboot2jpacrud/exception/GlobalExceptionHandler.java
+++ src/main/java/com/petros/springboot2jpacrud/exception/GlobalExceptionHandler.java
@@ -5,10 +5,31 @@
  */
 package com.petros.springboot2jpacrud.exception;
 
+import java.util.Date;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
 /**
  *
  * @author petros
  */
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
     
+    /*
+        To use ErrorDetails to return the error response, 
+        let’s create a GlobalExceptionHandler class 
+        annotated with @ControllerAdvice annotation. This 
+        class handles exception specific and global exceptions 
+        in a single place.
+
+    */
+    
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
