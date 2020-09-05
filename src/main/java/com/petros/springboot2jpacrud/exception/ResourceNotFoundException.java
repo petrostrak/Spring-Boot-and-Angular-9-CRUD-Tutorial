a05f979129b0a2c63ac7038d31715a6ab87911fe
petros <pit.trak@gmail.com>
2020-09-05 19:41:15 +0300

    Added ResourceNotFoundException

--- src/main/java/com/petros/springboot2jpacrud/exception/ResourceNotFoundException.java
+++ src/main/java/com/petros/springboot2jpacrud/exception/ResourceNotFoundException.java
@@ -5,10 +5,20 @@
  */
 package com.petros.springboot2jpacrud.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  *
  * @author petros
  */
-public class ResourceNotFoundException {
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
     
+    private static final long serialVersionUID = 1L;
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
