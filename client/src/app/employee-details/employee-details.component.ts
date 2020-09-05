bf8c1f8feb9507871018c68e5d77431304f51447
petros <pit.trak@gmail.com>
2020-09-05 21:57:02 +0300

    Created employee-details and employee.service

--- client/src/app/employee-details/employee-details.component.ts
+++ client/src/app/employee-details/employee-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { EmployeeListComponent } from './../employee-list/employee-list.component';
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../employee';
+import { Router, ActivatedRoute } from '@angular/router'
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number
+  employee: Employee
+
+  constructor(private route: ActivatedRoute, private router: Router, private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.employee = new Employee()
+
+    this.id = this.route.snapshot.params['id']
+
+    this.employeeService.getEmployee(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.employee = data
+      }, error => console.log(error))
+  }
+
+  list() {
+    this.router.navigate(['employees'])
   }
 
 }
